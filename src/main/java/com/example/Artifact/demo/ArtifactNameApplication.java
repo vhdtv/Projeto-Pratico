67984a54e4943c1c5feedc1d4414a2ca33f4bcf2
ocai0 <caioalvesfernandes@gmail.com>
2024-05-26 20:49:14 -0300

    Adicionado estrutura inicial feita pelo spring initializr

---
+++ src/main/java/com/example/Artifact/demo/ArtifactNameApplication.java
@@ -0,0 +1,13 @@
+package com.example.Artifact.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Artifact/demo/ArtifactNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Artifact.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
