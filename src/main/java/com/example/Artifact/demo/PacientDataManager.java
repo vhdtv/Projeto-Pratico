faad59e6d2420d9b94ede3cbc34c3f4d66e529fb
ocai0 <caioalvesfernandes@gmail.com>
2024-05-26 22:28:59 -0300

    Alterado nome do projeto e renomeado arquivo de entrada da aplicação

--- src/main/java/com/example/Artifact/demo/PacientDataManager.java
+++ src/main/java/com/example/Artifact/demo/PacientDataManager.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArtifactNameApplication {
+public class PacientDataManager {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArtifactNameApplication.class, args);
+		SpringApplication.run(PacientDataManager.class, args);
 	}
 
 }
