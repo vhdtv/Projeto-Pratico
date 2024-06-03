97ecbeadf5ed3b27c70a9468cdad0db50408c8f8
ocai0 <caioalvesfernandes@gmail.com>
2024-06-02 22:57:06 -0300

    Adicionado código para fazer o projeto ler os arquivos Model

--- src/main/java/com/example/artifact/demo/PacientDataManager.java
+++ src/main/java/com/example/artifact/demo/PacientDataManager.java
@@ -2,8 +2,12 @@ package com.example.artifact.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.artifact")
+@EntityScan(basePackages = "com.example.artifact.models")
 public class PacientDataManager {
 
 	public static void main(String[] args) {
