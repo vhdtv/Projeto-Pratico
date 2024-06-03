ade403a2778356a447015f6f8f9742defb7489a9
ocai0 <caioalvesfernandes@gmail.com>
2024-06-02 22:58:52 -0300

    Adicionado arquivo para criação da tabela tb_address

---
+++ src/main/java/com/example/artifact/models/AddressModel.java
@@ -0,0 +1,21 @@
+package com.example.artifact.models;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_ADDRESS")
+public class AddressModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String first_line;
+    private String house_number;
+    private String residence_type;
+    private String second_line; // acts like the neighborhood
+    private String city;
+    private String state;
+
+}
