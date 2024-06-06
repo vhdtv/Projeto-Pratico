1ffeeeb8dd6a17168615d0741ab7450b06dabb20
ocai0 <caioalvesfernandes@gmail.com>
2024-06-05 21:37:32 -0300

    Adicionado lógica para criar um novo registro de gênero

--- src/main/java/com/example/controllers/GenreController.java
+++ src/main/java/com/example/controllers/GenreController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,6 +32,9 @@ public class GenreController {
     @PostMapping("/genre")
     public ResponseEntity<GenreModel> saveGenre(@RequestBody @Valid GenreRecordDto genreRecordDto) {
         GenreModel genreModel = new GenreModel();
+        int lastIndexOnDatabase = this.genreRepository.findAll(Sort.by(Sort.Direction.DESC, "id")).get(0).getId();
+        genreModel.setId(lastIndexOnDatabase + 1);
+        System.out.println(genreRecordDto);
         BeanUtils.copyProperties(genreRecordDto, genreModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(genreRepository.save(genreModel));
     }
