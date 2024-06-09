d6721bbac712f2246bc0ea957efbad9a9414f31b
Paulo Vítor <paulovitoralsp@yahoo.com.br>
2024-06-09 20:35:27 -0300

    Adicionados os Getters and Setters.

--- src/main/java/com/example/models/SymptomModel.java
+++ src/main/java/com/example/models/SymptomModel.java
@@ -13,4 +13,23 @@ public class SymptomModel implements Serializable {
     private UUID uuid;
     private String name;
     private int weight;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }   
 }
