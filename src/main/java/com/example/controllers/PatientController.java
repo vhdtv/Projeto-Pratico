7d02c0239c3891bc925954b9800e7e3aee2cf0f3
ocai0 <caioalvesfernandes@gmail.com>
2024-06-09 16:39:45 -0300

    Adicionada lógica para salvar um Patient

--- src/main/java/com/example/controllers/PatientController.java
+++ src/main/java/com/example/controllers/PatientController.java
@@ -1,5 +1,8 @@
 package com.example.controllers;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dto.PatientRecordDto;
+import com.example.models.GenreModel;
 import com.example.models.PatientModel;
+import com.example.repositories.GenreRepository;
 import com.example.repositories.PatientRepository;
 
 import jakarta.validation.Valid;
@@ -20,11 +25,30 @@ import jakarta.validation.Valid;
 public class PatientController {
     @Autowired
     PatientRepository patientRepository;
+    @Autowired
+    GenreRepository genreRepository;
 
     @PostMapping("/patient")
     public ResponseEntity<PatientModel> savePatient(@RequestBody @Valid PatientRecordDto patientRecordDto) {
         PatientModel patientModel = new PatientModel(UUID.randomUUID());
         BeanUtils.copyProperties(patientRecordDto, patientModel);
+
+        patientModel.setBirthday(this.generateBirthdayValue(patientRecordDto.birthday()));
+        GenreModel genre = genreRepository.findById(patientRecordDto.genre()).orElse(null);
+        if (genre == null)
+            throw new Error("Genre was not found, received: " + patientRecordDto.genre());
+        patientModel.setGenre(genre);
         return ResponseEntity.status(HttpStatus.CREATED).body(patientRepository.save(patientModel));
+    }
+
+    private Date generateBirthdayValue(String inputDate) {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+        java.util.Date date = new java.util.Date();
+        try {
+            date = dateFormatter.parse(inputDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return new Date(date.getTime());
     }
 }
