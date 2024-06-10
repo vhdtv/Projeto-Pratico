68314dfc8153e8f3a8f968575dfec0ae07c14e38
Paulo Vítor <paulovitoralsp@yahoo.com.br>
2024-06-09 21:20:24 -0300

    Adicionado o método 'Post'.

--- src/main/java/com/example/controllers/SymptomController.java
+++ src/main/java/com/example/controllers/SymptomController.java
@@ -2,13 +2,21 @@ package com.example.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dto.SymptomRecordDto;
 import com.example.models.SymptomModel;
 import com.example.repositories.SymptomRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class SymptomController {
@@ -18,6 +26,13 @@ public class SymptomController {
     @GetMapping("/symptom")
     public ResponseEntity<List<SymptomModel>> listSymptoms() {
         return ResponseEntity.status(HttpStatus.OK).body(symptomRepository.findAll());
+    }
+    
+    @PostMapping("/symptom")
+    public ResponseEntity<SymptomModel> saveSymptom(@RequestBody @Valid SymptomRecordDto symptomRecordDto) {
+        SymptomModel symptomModel = new SymptomModel();
+        BeanUtils.copyProperties(symptomRecordDto, symptomModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(symptomRepository.save(symptomModel));
     }
 
 }
