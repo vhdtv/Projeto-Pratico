33b7492309da30cd26cade9ecf298e02de7232e3
Paulo Vítor <paulovitoralsp@yahoo.com.br>
2024-06-09 21:35:50 -0300

    Adicionado Get para buscar sintoma por UUID.

--- src/main/java/com/example/controllers/SymptomController.java
+++ src/main/java/com/example/controllers/SymptomController.java
@@ -1,12 +1,15 @@
 package com.example.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +29,11 @@ public class SymptomController {
     @GetMapping("/symptom")
     public ResponseEntity<List<SymptomModel>> listSymptoms() {
         return ResponseEntity.status(HttpStatus.OK).body(symptomRepository.findAll());
+    }
+
+    @GetMapping("/symptom/{id}")
+    public ResponseEntity<Optional<SymptomModel>> getSymptom(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.OK).body(symptomRepository.findById(id));
     }
     
     @PostMapping("/symptom")
