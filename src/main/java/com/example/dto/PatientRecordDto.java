52aae8018c0448f6f2ec5e67502bd491ef86c8f8
ocai0 <caioalvesfernandes@gmail.com>
2024-06-10 20:21:55 -0300

    Alterado tipo de dados para o PatientRecordDto

--- src/main/java/com/example/dto/PatientRecordDto.java
+++ src/main/java/com/example/dto/PatientRecordDto.java
@@ -2,7 +2,9 @@ package com.example.dto;
 
 import java.util.Optional;
 
+import com.example.models.AddressModel;
+
 public record PatientRecordDto(String name, String motherName, String birthday,
-        int genre, Optional<Integer> address, Optional<String> phoneNumber, Optional<String> email) {
+                int genre, Optional<AddressModel> address, Optional<String> phoneNumber, Optional<String> email) {
 
 }
