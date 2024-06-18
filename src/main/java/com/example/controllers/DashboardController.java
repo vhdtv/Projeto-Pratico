be7809ca6f0efc854a29620222a722af6a1ea1a6
Vinicius Garcia Silva <129995933+VinixGarciaSilva@users.noreply.github.com>
2024-06-18 20:30:31 -0300

    Update  comments DashboardController.java

--- src/main/java/com/example/controllers/DashboardController.java
+++ src/main/java/com/example/controllers/DashboardController.java
@@ -17,6 +17,10 @@ import com.example.models.SymptomModel;
 import com.example.repositories.AttendanceRegistrationRepository;
 import com.example.repositories.PriorityRepository;
 
+/**
+ * Controlador para gerenciar o dashboard Controller.
+ */
+
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
@@ -27,13 +31,21 @@ public class DashboardController {
 
     @GetMapping({ "/", "" })
     public String page(Model context) {
+
+        // Busca todos os registros de atendimento
         List<ReportModel> attendanceRegistrations = this.attendanceRegistrationRepository.findAll();
         context.addAttribute("lastAttendanceRegistrations", attendanceRegistrations);
+
+        // Calcula o total de pacientes 
         int totalPacients = (int) this.attendanceRegistrationRepository.count();
         context.addAttribute("totalPatients", totalPacients);
+
+        // Conta o total de pacientes com alta prioridade
         int totalHighPriorityPatients = this.attendanceRegistrationRepository
                 .findAllByPriorityEquals(this.priorityRepository.findById(1).get()).size();
         context.addAttribute("totalHighPriorityPatients", totalHighPriorityPatients);
+
+        // Calcula o tempo de atendimento
         long totalTime = 0;
         for (ReportModel register : attendanceRegistrations) {
             Date updatedAt = register.getUpdatedAt();
@@ -44,6 +56,8 @@ public class DashboardController {
             long difference = updatedAt.getTime() - creaDateAt.getTime();
             totalTime += difference;
         }
+
+        // Calcula o tempo médio de atendimento e adiciona ao modelo
         int numberOfRegistrations = attendanceRegistrations.size();
         String averageTime = "N/A";
         if (totalTime != 0) {
@@ -54,6 +68,7 @@ public class DashboardController {
         }
         context.addAttribute("averageTime", averageTime);
 
+        // Calcula a frequência dos sintomas
         Map<SymptomModel, Integer> frequencySymptoms = new HashMap<>();
         for (ReportModel register : attendanceRegistrations) {
             for (SymptomModel symptom : register.getSymptoms()) {
@@ -61,6 +76,7 @@ public class DashboardController {
             }
         }
 
+        // Define qual o sintoma esta mais frequente
         SymptomModel highSymptom = frequencySymptoms.entrySet().stream()
                 .max((entry1, entry2) -> {
                     int frequencyCompare = entry1.getValue().compareTo(entry2.getValue());
@@ -73,7 +89,10 @@ public class DashboardController {
                 .map(Map.Entry::getKey)
                 .orElse(null);
 
+                // Adiciona o sintoma mais frequente ao model
                 context.addAttribute("highSymptom", highSymptom != null ? highSymptom.getName() : null);
+
+        // Retorna a view do dashboard
         return "pages/dashboard";
     }
 }
